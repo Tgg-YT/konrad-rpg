90a87eceaeffdf7207d726f84fe67b4cdeb5f1bb
Tgg-YT <konradek.derewecki@gmail.com>
2020-12-08 19:23:17 +0100

    Adding Blend Tree Running

---
+++ Assets/PlayerRunning.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRunning : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+    }
+
+    public void Running()
+    {
+        animator.SetTrigger("Running");
+    }
+}
